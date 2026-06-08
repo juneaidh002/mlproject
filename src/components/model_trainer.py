49fbefd6f4dcfd4f87c3cd8842db3b9b11c1fc0f
Juneaidh002 <sjmohammad002@gmail.com>
2026-06-08 23:05:19 +0530

    pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,85 +47,72 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regression" : KNeighborsRegressor(),
                 "XGBoosting Regression" : XGBRegressor(),
-                "CatBoosting Refression" : CatBoostRegressor(verbose=True),
-                "AdaBoost Classifier" : AdaBoostRegressor()
+                "CatBoosting Regression" : CatBoostRegressor(verbose=True),
+                "AdaBoost Regressor" : AdaBoostRegressor()
             }
 
 
             param_grids = {
 
                 "Random Forest": {
-                    "n_estimators": [100, 200, 500],
-                    "max_depth": [None, 10, 20, 30],
-                    "min_samples_split": [2, 5, 10],
-                    "min_samples_leaf": [1, 2, 4],
-                    "max_features": ["sqrt", "log2", None]
+                    "n_estimators": [100, 200],
+                    "max_depth": [10, 20],
+                    "min_samples_split": [2, 5]
                 },
 
                 "Decision Tree": {
-                    "criterion": ["squared_error", "friedman_mse", "absolute_error"],
-                    "max_depth": [None, 5, 10, 20, 30],
-                    "min_samples_split": [2, 5, 10],
-                    "min_samples_leaf": [1, 2, 4],
-                    "max_features": ["sqrt", "log2", None]
+                    "max_depth": [5, 10, 20],
+                    "min_samples_split": [2, 5]
                 },
 
                 "Gradient Boosting": {
-                    "n_estimators": [100, 200, 500],
-                    "learning_rate": [0.01, 0.05, 0.1, 0.2],
-                    "max_depth": [3, 5, 7],
-                    "min_samples_split": [2, 5, 10],
-                    "subsample": [0.8, 0.9, 1.0]
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.05, 0.1],
+                    "max_depth": [3, 5]
                 },
 
                 "Linear Regression": {
-                    # No major hyperparameters to tune
-                    # Can use fit_intercept and positive
-                    "fit_intercept": [True, False],
-                    "positive": [True, False]
+                    "fit_intercept": [True, False]
                 },
 
                 "K-Neighbors Regression": {
-                    "n_neighbors": [3, 5, 7, 9, 11, 15],
-                    "weights": ["uniform", "distance"],
-                    "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
-                    "p": [1, 2]  # Manhattan, Euclidean
+                    "n_neighbors": [3, 5, 7, 9],
+                    "weights": ["uniform", "distance"]
                 },
 
                 "XGBoosting Regression": {
-                    "n_estimators": [100, 200, 500],
-                    "learning_rate": [0.01, 0.05, 0.1],
-                    "max_depth": [3, 5, 7, 10],
-                    "subsample": [0.8, 0.9, 1.0],
-                    "colsample_bytree": [0.8, 0.9, 1.0],
-                    "gamma": [0, 0.1, 0.3],
-                    "reg_alpha": [0, 0.01, 0.1],
-                    "reg_lambda": [1, 1.5, 2]
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.05, 0.1],
+                    "max_depth": [3, 5, 7],
+                    "subsample": [0.8, 1.0]
                 },
 
                 "CatBoosting Regression": {
-                    "iterations": [100, 300, 500],
-                    "learning_rate": [0.01, 0.05, 0.1],
-                    "depth": [4, 6, 8, 10],
-                    "l2_leaf_reg": [1, 3, 5, 7],
-                    "border_count": [32, 64, 128]
+                    "iterations": [100, 300],
+                    "learning_rate": [0.05, 0.1],
+                    "depth": [4, 6, 8]
                 },
 
                 "AdaBoost Regressor": {
-                    "n_estimators": [50, 100, 200, 500],
-                    "learning_rate": [0.01, 0.05, 0.1, 1.0],
-                    "loss": ["linear", "square", "exponential"]
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.05, 0.1]
                 }
             }
-            model_report:dict = evaluate_models(X_train=x_train,y_train=y_train,X_test = x_test,
-                                               y_test = y_test,models=models)
-            best_model_score = max(sorted(model_report.values()))
+            model_report, trained_models = evaluate_models(
+                X_train=x_train,
+                y_train=y_train,
+                X_test=x_test,
+                y_test=y_test,
+                models=models,
+                param=param_grids
+                )           
+            best_model_score = max(model_report.values())
 
             best_model_name = list(model_report.keys())[ 
                 list(model_report.values()).index(best_model_score)
             ]
 
-            best_model = models[best_model_name]
+            best_model = trained_models[best_model_name]
 
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,13 @@
-import os 
+import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 import dill
 import pickle
-
 from sklearn.metrics import r2_score
 from sklearn.model_selection import RandomizedSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -21,23 +21,45 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
+        trained_models = {}
+
         for i in range(len(list(models))):
+
             model = list(models.values())[i]
-            model.fit(X_train,y_train)
+            para = param[list(models.keys())[i]]
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            gs = RandomizedSearchCV(
+                model,
+                para,
+                n_iter=20,
+                cv=3,
+                n_jobs=-1,
+                random_state=42
+            )
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            gs.fit(X_train, y_train)
 
-            test_model_score = r2_score(y_test,y_test_pred)
+            best_model = gs.best_estimator_
 
-            report[list(models.keys())[i]] = test_model_score
-           
-        return report
+            y_test_pred = best_model.predict(X_test)
+
+            report[list(models.keys())[i]] = r2_score(y_test, y_test_pred)
+
+            trained_models[list(models.keys())[i]] = best_model
+
+        return report, trained_models
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
